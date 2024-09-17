7765621042a2d78b7357d0d8f2bc04c0273b915f
Catherine Patchell <catherine.patchell@turo.com>
2024-09-16 23:27:56 -0700

    feat: use NavLink and extract social links into own component

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './base.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Weddings from './routes/Weddings';
+import Contact from './routes/Contact';
+import Home from './routes/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "weddings",
+    element: <Weddings />,
+  },
+  {
+    path: "about",
+    element: <Contact />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
